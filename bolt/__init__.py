58f138aacff531abd9f3b6e67610f1fcd3547534
nnasrrull <nasrul@syntwin.ai>
2025-11-08 13:37:52 +0100

    Resolved build errors

--- bolt/__init__.py
+++ bolt/__init__.py
@@ -1,4 +1,4 @@
-from bolt_client import Client, create_client
-from bolt_schemas import FleetOrder, Vehicle, PortalStatus, Driver, FleetStateLog
+from .bolt_client import Client, create_client
+from .bolt_schemas import FleetOrder, Vehicle, PortalStatus, Driver, FleetStateLog
 
 __all__ = ["Client", "create_client", "FleetOrder", "Vehicle", "PortalStatus", "Driver", "FleetStateLog"]
